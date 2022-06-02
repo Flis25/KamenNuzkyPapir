8f73226cc79b00ec7848ed35cd6717b0f845fbef
marek.fleischmann <marek.fleischmann@VT-8>
2022-06-02 13:29:55 +0200

    upgrade všeho a mini bug fix i guess.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,189 @@
+﻿using Spectre.Console;
+
+bool start = false;
+Console.WriteLine("Chceš si zahrát? (ano) (ne)");
+string dzosef = Console.ReadLine();
+dzosef.ToLower();
+if (dzosef == "ano")
+{
+    AnsiConsole.Write(
+        new FigletText("KÁMEN NŮŽKY PAPÍR !")
+            .Centered()
+            .Color(Color.Red));
+    Console.ResetColor();
+    Console.WriteLine("HRÁČ 1 [NOT READY] (zmáčnki cokoliv ke startu)");
+    Console.ReadKey();
+    Console.Clear();
+    Console.WriteLine("Hráč 1 [READY]");
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Čekejte prosím");
+    await AnsiConsole.Progress()
+        .StartAsync(async ctx =>
+        {
+            var task1 = ctx.AddTask("[green]LOADING[/]");
+
+            while (!ctx.IsFinished)
+            {
+                await Task.Delay(111);
+                task1.Increment(1.5);
+            }
+        });
+    Console.WriteLine("Cool loading, right ? Ne? ... Byl tam jen pro swag :(((");
+    Thread.Sleep(3300);
+    Console.Clear();
+
+    int hrac1 = 0;
+    int hrac2 = 0;
+    bool ne = true;
+
+    while (ne == true)
+    {
+        Console.WriteLine("Vyber si mezi kámen (1) , nůžky (2) , papír (3)");
+        string slovo = Console.ReadLine();
+        if (slovo == "1")
+        {
+            Console.WriteLine("Zvolil si kámen.");
+            Random random = new Random();
+            int ai = random.Next(1, 4);
+            if (ai == 1)
+            {
+                Console.WriteLine("Tvůj oponent zvolil kámen");
+                Console.WriteLine("REMÍZA");
+            }
+            else if (ai == 2)
+            {
+                Console.WriteLine("Tvůj oponent zvolil nůžky");
+                Console.WriteLine("VYHRÁVÁŠ");
+                hrac1 = hrac1 + 1;
+            }
+            else if (ai == 3)
+            {
+                Console.WriteLine("Tvůj oponent zvolil papír");
+                Console.WriteLine("PROHRÁLS");
+                hrac2 = hrac2 + 1;
+            }
+        }
+        else if (slovo == "2")
+        {
+            Console.WriteLine("Zvolil si nůžky.");
+            Random random = new Random();
+            int ai = random.Next(1, 4);
+            if (ai == 1)
+            {
+                Console.WriteLine("Tvůj oponent zvolil kámen");
+                Console.WriteLine("PROHRÁLS");
+                hrac2 = hrac2 + 1;
+            }
+            else if (ai == 2)
+            {
+                Console.WriteLine("Tvůj oponent zvolil nůžky");
+                Console.WriteLine("REMÍZA");
+            }
+            else if (ai == 3)
+            {
+                Console.WriteLine("Tvůj oponent zvolil papír");
+                Console.WriteLine("VYHRÁVÁŠ");
+                hrac1 = hrac1 + 1;
+            }
+        }
+        else if (slovo == "3")
+        {
+            Console.WriteLine("Zvolil si papír.");
+            Random random = new Random();
+            int ai = random.Next(1, 4);
+            if (ai == 1)
+            {
+                Console.WriteLine("Tvůj oponent zvolil kámen");
+                Console.WriteLine("VYHRÁVÁŠ");
+                hrac1 = hrac1 + 1;
+            }
+            else if (ai == 2)
+            {
+                Console.WriteLine("Tvůj oponent zvolil nůžky");
+                Console.WriteLine("PROHRÁLS");
+                hrac2 = hrac2 + 1;
+            }
+            else if (ai == 3)
+            {
+                Console.WriteLine("Tvůj oponent zvolil papír");
+                Console.WriteLine("REMÍZA");
+            }
+        }
+        AnsiConsole.Write(new BarChart()
+            .Width(20)
+            .Label("[green bold underline]body[/]")
+            .CenterLabel()
+            .AddItem("TY", hrac1, Color.Red)
+            .AddItem("OPONENT", hrac2, Color.Blue1));
+        Console.ReadKey();
+        Console.ResetColor();
+        Console.Clear();
+        if (hrac1 == 5)
+        {
+            Console.WriteLine("Zvítězil si nad oponentem.");
+            Console.WriteLine("Chceš hrát znovu? (ano) (ne)");
+            ne = false;
+            string moznost = Console.ReadLine();
+            moznost = moznost.ToLower();
+            if (moznost == "ano")
+            {
+                ne = true;
+                Console.Clear();
+            }
+            else
+            {
+                ne = false;
+                Console.Clear();
+            }
+        }
+        else if (hrac2 == 5)
+        {
+            Console.WriteLine("Prohrál si nad oponentem.");
+            Console.WriteLine("Chceš hrát znovu? (ano) (ne)");
+            ne = false;
+            string moznost = Console.ReadLine();
+            moznost = moznost.ToLower();
+            if (moznost == "ano")
+            {
+                ne = true;
+                Console.Clear();
+            }
+            else
+            {
+                ne = false;
+                Console.Clear();
+            }
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Jsi si 100% jistý?");
+    string tf = Console.ReadLine();
+    tf.ToLower();
+    if (tf == "ano")
+    {
+        Console.WriteLine("Ale, když si nechceš zahrát... Nemůžeme být spolu. To vážně chceš?");
+        string tf2 = Console.ReadLine();
+        tf.ToLower();
+        if (tf2 == "ano")
+        {
+            Console.WriteLine("Dobrá tedy... Stačí zmáčknout klávesu... a je to. Do toho.");
+            Console.ReadKey();
+            for(int i = 0; i < 300; i++)
+            {
+                Console.WriteLine("ZLEJ SI !");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Takže ses rozmyslel ? No... Já už ale nechci.");
+            Thread.Sleep(2000);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Takže ses rozmyslel ? No... Já už ale nechci.");
+        Thread.Sleep(2000);
+    }
+}
